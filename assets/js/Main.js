3b6183812c0acefd54e0c5e00116f0192f155855
BrayanChavarro <brayanstik.21@outlook.com>
2022-09-20 06:35:37 -0500

    Ejemplo SAIA modal

---
+++ assets/js/Main.js
@@ -0,0 +1,30 @@
+/* if (typeof abrirModal !== 'undefined') {
+const openModal = document.querySelector('.hero__cta');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal__close');
+openModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.add('modal--show');
+});
+
+closeModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.remove('modal--show');
+});
+
+  }
+
+ */
+  const openModal = document.querySelector('.hero__cta');
+  const modal = document.querySelector('.modal');
+  const closeModal = document.querySelector('.modal__close');
+  
+  openModal.addEventListener('click', (e)=>{
+      e.preventDefault();
+      modal.classList.add('modal--show');
+  });
+  
+  closeModal.addEventListener('click', (e)=>{
+      e.preventDefault();
+      modal.classList.remove('modal--show');
+  });
